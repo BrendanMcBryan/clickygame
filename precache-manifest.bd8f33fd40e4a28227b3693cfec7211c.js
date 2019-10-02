e84b86a29ee239f2f5067d93665ae4d383890cc5
Brendan McBryan <brendan.mcbryan@gmail.com>
2019-10-01 23:05:43 -0400

    Updates

---
+++ precache-manifest.bd8f33fd40e4a28227b3693cfec7211c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b8e8527f91fb6499c9ab5dce38e1f2c",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "71ba0a003896bb311fec",
+    "url": "/clickygame/static/css/main.7980cc80.chunk.css"
+  },
+  {
+    "revision": "c219700819c347594ed9",
+    "url": "/clickygame/static/js/2.a6ac0812.chunk.js"
+  },
+  {
+    "revision": "71ba0a003896bb311fec",
+    "url": "/clickygame/static/js/main.0ce9447b.chunk.js"
+  },
+  {
+    "revision": "896782258aca16ff8f8d",
+    "url": "/clickygame/static/js/runtime-main.837f60ac.js"
+  }
+]);
